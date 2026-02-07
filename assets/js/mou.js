7821d795b6732b586e4b10a183994f467461db18
HarishSRM2025 <harishsrm25@gmail.com>
2026-02-07 15:22:31 +0530

    latest MOU

--- assets/js/mou.js
+++ assets/js/mou.js
@@ -10,12 +10,12 @@ const activity_content=[
         title:"Title-3",
     }
 ]
-activity_content.map((e,i)=>{
+for(let i=1;i<=36;i++){
     new_activities.innerHTML+=
     `<div class="collab-card">
         <div class="img">
-            <img src="./assets/images/collaboration/${i+1}.png" alt="">
+            <img src="./assets/images/collaboration/${i}.png" alt="">
         </div>
     </div>`
-})
+}
     
